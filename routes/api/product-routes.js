0e8331e256c8dc92b6fcb37c5ebfae351774defa
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-27 07:31:33 -0500

    updated delete route in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,23 +113,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-//   try {
-//     const productData = await Product.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     });
-//     console.log(productData);
-//     if (!productData[0]) {
-//       res.status(404).json({ message: 'No category with this id!' });
-//       return;
-//     }
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
